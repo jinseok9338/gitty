055468dd04b539ab46a4c7a80a9cb80977978ae1
Jinseok Seo <jinseok9338@gmail.com>
2023-01-31 23:04:10 +0900

    fix precommit

--- precommit.sh
+++ precommit.sh
@@ -3,4 +3,7 @@
 echo "Running rustfmt..."
 cargo fmt
 echo "Running clippy..."
-cargo clippy --fix --allow-staged
+cargo clippy --fix --allow-staged -- \
+-W clippy::pedantic \
+-W clippy::nursery \
+-W clippy::unwrap_used \
